d23866ecea19ff5efc3b881fd68a48e82491981e
hemanga-rimal <45951761+hemanga-rimal@users.noreply.github.com>
2022-06-17 19:45:32 -0700

    new leet

---
+++ leetjava/stringparagraph.java
@@ -0,0 +1,21 @@
+public class solution {
+    int numTimes(String paragraph, String word) {
+      
+      String n[] = paragraph.split(" ");
+      
+      int wordCount = 0;
+      for (int i = 0; i < n.length; i++) {
+      
+      if (word.equals(n[i]))
+        wordCount++;
+      }
+      return wordCount;
+    }
+  
+  public static void main(String args[]) {
+    String paragraph = "what is going on?";
+    String word = "what";
+    System.out.println(numTimes(paragraph, word));
+  }
+  
+}
